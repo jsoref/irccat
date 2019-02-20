20fe4a2fdb86157640428cc22ddb3d71473573f3
Peter van Dijk <peter.van.dijk@powerdns.com>
2019-02-20 14:38:50 +0100

    prettier irc truncation for github commits

--- util/string.go
+++ util/string.go
@@ -9,18 +9,14 @@ func Truncate(in string, length int) string {
 	parts := strings.Split(in, "\n")
 	in = parts[0]
 	if len(in) <= length {
-		if len(parts) > 1 {
-			return in + "…"
-		} else {
 			return in
-		}
 	}
 
 	runes := []rune(in)
 
 	for i := len(runes) - 1; i > 1; i-- {
 		if unicode.IsSpace(runes[i]) && len(string(runes[:i])) < length {
-			return string(runes[:i]) + "…"
+			return string(runes[:i]) + " ..."
 		}
 	}
 
